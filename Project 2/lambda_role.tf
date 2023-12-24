1b7eeef1e4516214a08fc88a0bd32310440d9418
Georgia Omoja <gerogiaomoja@gmail.com>
2023-12-24 23:53:51 +0100

    Serverless Sending Application

--- Project 2/lambda_role.tf
+++ Project 2/lambda_role.tf
@@ -1,6 +1,6 @@
 // Creates Lambda role and assign it to AWS Lambda Service
-resource "aws_iam_role" "aws_lambda_role" {
-  name = "lamda_role"
+resource "aws_iam_role" "lambda_role" {
+  name = "my-lambda-role"
 
   assume_role_policy = <<EOF
 {
@@ -21,17 +21,17 @@ EOF
 
 // Attach the required permissions to the role
 resource "aws_iam_role_policy_attachment" "ses_full_access" {
-  role       = aws_iam_role.my_role.name
+  role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSESFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "sns_full_access" {
-  role       = aws_iam_role.my_role.name
+  role       = aws_iam_role.lamdba_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "step_functions_full_access" {
-  role       = aws_iam_role.my_role.name
+  role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSStepFunctionsFullAccess"
 }
 
